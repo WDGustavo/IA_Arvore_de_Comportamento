0629f5e28b11e552cf057842dc889ff9a4fd3080
WDGustavo <54276455+WDGustavo@users.noreply.github.com>
2021-05-17 09:14:58 -0300

    Parte1_Movimentaçao_Aleatoria
    Criação do script Wander.BT e as decisões que o tanque preto vai fazer, nessa parte foi feita a escolha aleatoria de posição e sua movimentação ate esse ponto

---
+++ RobotGuardBTStarter/Assets/AI.cs
@@ -0,0 +1,92 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.AI;
+using Panda;
+
+public class AI : MonoBehaviour
+{
+    //criação das variaveis
+    public Transform player;
+    public Transform bulletSpawn;
+    public Slider healthBar;   
+    public GameObject bulletPrefab;
+
+    NavMeshAgent agent;
+    public Vector3 destination; // The movement destination.
+    public Vector3 target;      // The position to aim to.
+    float health = 100.0f;
+    float rotSpeed = 5.0f;
+
+    float visibleRange = 80.0f;
+    float shotRange = 40.0f;
+    
+
+    void Start()
+    {
+        //pega o componente navmesh do asset 
+        agent = this.GetComponent<NavMeshAgent>();
+        //faz ele ir diminuindo a velocidade quando estiver chegando no distino
+        agent.stoppingDistance = shotRange - 5; //for a little buffer
+        //sempre faz o update da vida do npc
+        InvokeRepeating("UpdateHealth",5,0.5f);
+    }
+
+    void Update()
+    {
+        //posiciona a barra de vida de acordo com a camera
+        Vector3 healthBarPos = Camera.main.WorldToScreenPoint(this.transform.position);
+        //seta o valor da vida como a variavel health
+        healthBar.value = (int)health;
+        //coloca a barra de vida em cima do npc
+        healthBar.transform.position = healthBarPos + new Vector3(0,60,0);
+    }
+
+    void UpdateHealth()
+    {
+        //se a vida for menos que 100 recupera ela
+       if(health < 100)
+        health ++;
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        //caso colide com o projetil perca 10 de vida
+        if(col.gameObject.tag == "bullet")
+        {
+            health -= 10;
+        }
+    }
+
+    //cria o metodo de ação da arvore de decisões de escolher uma posição aleatoria
+    [Task]
+    public void PickRandomDestination()
+    {
+        //cria a variavel que vai escolher uma posição aleatoria entre -100 e 100 nos eixo X e Z
+        Vector3 dest = new Vector3(Random.Range(-100, 100), 0, Random.Range(-100, 100));
+        //seta o destino dele para a posição escolhida aleatoriamente
+        agent.SetDestination(dest);
+        //fala que a ação foi concluida com sucesso
+        Task.current.Succeed();
+    }
+
+    //cria o metodo de ação da arvore de decisões para se mover para a posição escolhida
+    [Task]
+    public void MoveToDestination()
+    {
+        //se estiver rodando a aplicação
+        if (Task.isInspected)
+        {
+            //cira um timer para ver o tempo rodando
+            Task.current.debugInfo = string.Format("t={0:0.00}", Time.time);
+        }
+        //se estiver chegado no local
+        if (agent.remainingDistance <= agent.stoppingDistance && !agent.pathPending)
+        {
+            //fala que a ação foi concluida com sucesso
+            Task.current.Succeed();
+        }
+    }
+}
+
---
+++ RobotGuardBTStarter/Assets/Drive.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drive : MonoBehaviour {
+    //criação das variaveis
+	float speed = 20.0F;
+    float rotationSpeed = 120.0F;
+    public GameObject bulletPrefab;
+    public Transform bulletSpawn;
+
+    void Update() {
+        //faz ele usar os inputs setado na vertical
+        float translation = Input.GetAxis("Vertical") * speed;
+        //faz ele usar os inputs setado na horizontal
+        float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
+        //faz o calculo pra ele se mover nas verticais com o deltaTime
+        translation *= Time.deltaTime;
+        //faz o calculo pra ele se mover nas horizontais com o deltaTime
+        rotation *= Time.deltaTime;
+        //fala o eixo que o translation vai interferir no caso o z
+        transform.Translate(0, 0, translation);
+        //fala o eixo que o rotation vai interferir no caso o y
+        transform.Rotate(0, rotation, 0);
+        //ao apertar a barra de espaço atira um projetiol
+        if(Input.GetKeyDown("space"))
+        {
+            //instancia o projetil
+            GameObject bullet = GameObject.Instantiate(bulletPrefab, bulletSpawn.transform.position, bulletSpawn.transform.rotation);
+            //da a força de tiro do projetil
+            bullet.GetComponent<Rigidbody>().AddForce(bullet.transform.forward*2000);
+        }
+    }
+}
